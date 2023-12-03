f49f9c3209724f67593faff99dab7b000f4d5618
andrey <tritg@yandex.ru>
2023-12-03 18:59:32 +0300

    added screnn two files

--- hw_9/script.js
+++ hw_9/script.js
@@ -1,12 +1,10 @@
 'use strict'
 // 1. При изменении значения в input с id="from", значение содержащееся в нем должно моментально отображаться в span. То есть при печати в input'е тег span также должен меняться.
-
 let inputForm = document.getElementById('from');
 
 inputForm.addEventListener('input', function (event) {
     document.querySelector('span').innerHTML = event.target.value
 });
-
 // 2. При клике на кнопку с классом messageBtn необходимо элементу с классом message: - добавить два класса: animate_animated и animate_fadeInLeftBig
 // - поставить данному элементу стиль visibility в значение 'visible'.
 let elBtn = document.querySelector('.messageBtn');
@@ -16,7 +14,6 @@ elBtn.addEventListener('click', () => {
     elBtn.style.visibility = 'visible';
 
 });
-
 // 3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме.Если какое - либо поле не заполнено, форма не должна отправляться, также должны быть подсвечены незаполненные поля(необходимо поставить класс error незаполненным полям).Как только пользователь начинает заполнять какое - либо поле, необходимо, при вводе в данное поле, произвести проверку:
 // - Если поле пустое, необходимо данное поле подсветить(поставить класс error данному полю).
 // - Если поле было чем - либо заполнено, подсветку(класс error) необходимо убрать.
