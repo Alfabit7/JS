8eb2c72cd84827bd55c59432fa9d59fcc185a6c7
andrey <tritg@yandex.ru>
2023-12-10 19:44:46 +0300

    added folder hw_11

---
+++ hw_11/data.js
@@ -0,0 +1,31 @@
+const dataProductsJson = `[
+    {
+    "img": "images/prod_1.jpg",
+    "title": "MANGO PEOPLE T-SHIRT",
+    "price": "$300",
+    "color": "Red",
+    "size": "Xl",
+    "quantity": "2"
+},
+{
+    "img": "./images/prod_2.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "price": "$500",
+    "color": "Grey",
+    "size": "XXL",
+    "quantity": "22"
+},
+{
+    "img": "https://porsche-design.com.ru/image/cache/catalog/ochki/2020/p8693_black_01-1000x1000.jpg",
+    "title": "GLASS PORSHE",
+    "price": "$1500",
+    "color": "Black",
+    "size": "57",
+    "quantity": "14"
+}
+
+]`
+
+
+
+
---
+++ hw_11/script.js
@@ -0,0 +1,43 @@
+const dataProducts = JSON.parse(dataProductsJson);
+
+let container = document.query
+
+/* HTML Теги скрипта при использовании querySelector не анализируются как HTML 
+поэтому получить вложенный элемент, заключенный в тег Script  напрямую вот так: let productTemplate = document.querySelector('#productTemplate .product')  невозможно */
+//Получаем элемент  шаблона
+let productTemplate = document.querySelector('#productTemplate');
+//Получаем содержимое скрипта шаблона
+let templateContent = productTemplate.innerHTML;
+// Создаем временный контейнер 
+let tempContainer = document.createElement('div');
+
+// Устанавливаем внутренний HTML-код контейнера с содержимым шаблона
+tempContainer.innerHTML = templateContent;
+// Теперь   выбираем элемент с классом Product внутри контейнера
+let productElement = tempContainer.querySelector('.product');
+
+
+dataProducts.forEach(element => {
+    //Клонируем ноду product
+    let product = productElement.cloneNode(true);
+
+    product.querySelector('.product__img img').src = element.img;
+    product.querySelector('.product__img img').alt = 'photo_product';
+
+    let price = product.querySelector(".product__info .product__price .product__property-price");
+    price.textContent = price.textContent + element.price;
+
+    let title = product.querySelector(".product__info .product__title");
+    title.textContent = element.title;
+
+    let color = product.querySelector(".product__info .product__color .product__property");
+    color.textContent = color.textContent + element.color;
+
+    let size = product.querySelector(".product__info .product__size .product__property");
+    size.textContent = size.textContent + element.size;
+
+    let quantity = product.querySelector(".product__info .product__quantity .product__property-quantity");
+    quantity.textContent = quantity.textContent + element.quantity;
+
+    container.appendChild(product);
+});
